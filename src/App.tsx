493c6b67a58ce981b6d42394ea77b3d679c669a3
Cleberton Kobay <Clebertonkobay@gmail.com>
2024-02-11 18:31:09 -0300

    adição da função de salvar e busca

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,49 @@
+import { ChangeEvent, useState } from 'react'
 import logo from './asset/Logo.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
+interface Note {
+  id: string,
+  date: Date,
+  content: string
+}
+
 export function App() {
+  const [search, setSearch] = useState('');
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notesOnStorage = localStorage.getItem('notes')
+
+    if (notesOnStorage) {
+      return JSON.parse(notesOnStorage);
+    }
+    return []
+  });
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      content,
+      date: new Date()
+    }
+
+    const notesArray = [newNote, ...notes];
+
+    setNotes(notesArray);
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+
+  }
+
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+    event.preventDefault();
+    const query = event.target.value;
+    setSearch(query)
+
+  }
+
+  const filteredNotes = search === '' ? notes : notes.filter((notes) => {
+    return notes.content.toLowerCase().includes(search.toLowerCase())
+  })
 
   return (
     <div className='mx-auto max-w-6xl my-12 space-y-6'>
@@ -13,6 +54,7 @@ export function App() {
           type="text"
           placeholder='Busque em suas notas'
           className='w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none'
+          onChange={handleSearch}
         />
       </form>
 
@@ -20,9 +62,11 @@ export function App() {
 
       <div className='grid grid-cols-3 auto-rows-[250px] gap-6'>
 
-        <NewNoteCard />
-        <NoteCard note={{date:new Date(), content:'Hello world'}}/>
-        
+        <NewNoteCard onNoteCreated={onNoteCreated} />{
+          filteredNotes.map((note) => {
+            return <NoteCard note={note} key={note.id} />
+          })
+        }
       </div>
     </div>
   )
